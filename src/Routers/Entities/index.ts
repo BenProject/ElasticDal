5f41a0b26c0d8e223e7a3b87af81dbcea0b30f21
Ben <hason.ben2@gmail.com>
2020-11-07 22:27:55 +0200

    changed route of get

--- src/Routers/Entities/index.ts
+++ src/Routers/Entities/index.ts
@@ -2,6 +2,6 @@ import * as express from "express";
 import entitiesCtrl from "./entities-controller";
 const router = express.Router();
 
-router.route("/").post(entitiesCtrl.createEntity).get(entitiesCtrl.getEntities);
-
+router.route("/").post(entitiesCtrl.createEntity);
+router.route("/search").post(entitiesCtrl.getEntities)
 export default router;
